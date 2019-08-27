f0b868bb4b808c6d7286c9a3ad2700f4d3084770
Matt Stuhring <whereyouatmatt@gmail.com>
2019-08-27 13:34:33 -0700

    Need to add /myprofile page

---
+++ src/main/java/com/mattstuhring/codefellowship/controllers/ApplicationUserController.java
@@ -0,0 +1,50 @@
+package com.mattstuhring.codefellowship.controllers;
+
+import com.mattstuhring.codefellowship.models.ApplicationUser;
+import com.mattstuhring.codefellowship.models.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.ArrayList;
+
+@Controller
+public class ApplicationUserController {
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+
+    @PostMapping("/users")
+    public RedirectView createUser(String username, String password, String firstName, String lastName, String dateOfBirth, String bio) {
+        ApplicationUser newUser = new ApplicationUser(username, encoder.encode(password), firstName, lastName, dateOfBirth, bio);
+        applicationUserRepository.save(newUser);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(newUser, null, new ArrayList<>());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new RedirectView("/");
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String getSignupPage() {
+        return "signup";
+    }
+
+    @GetMapping("/myprofile")
+    public String getProfilePage() {
+        return "myprofile";
+    }
+
+}
